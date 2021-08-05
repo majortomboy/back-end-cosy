dedf8caed8586c81ca77eee81ed3ba3c84702e09
Sidney DuPont <sidneyadupont@gmail.com>
2021-08-05 10:34:37 -0700

    Adds configuration for AWS S3 image storage

--- cosy_backend/settings.py
+++ cosy_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cosyapp',
     'corsheaders',
     'rest_framework',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,12 @@ MEDIA_URL = '/media/' # http://localhost:8000/media/
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
